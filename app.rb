03ca674a0411aff34a5a8b95ba6dc7dbe272a89e
cccswann <cccswann@gmail.com>
2021-02-04 18:35:27 -0800

    Complete

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        p "Hello world."
+    end
+
+    get '/name' do 
+        p "My name is Ciara"
+    end
+
+    get '/hometown' do
+        p "My hometown is San Ramon"
+    end
+
+    get '/favorite-song' do
+        p "My favorite song is IDK"
+    end
 end
